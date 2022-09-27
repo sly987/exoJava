97fa68eed663908ac54bfd1a75e85bf183e3e0f3
yweng <yweng@localhost>
2022-09-27 13:14:18 +0200

    exo 1 et cours

--- exoJava/src/TD2/Exo1.java
+++ exoJava/src/TD2/Exo1.java
@@ -1,10 +1,20 @@
 package TD2;
 
+import java.util.Scanner;
+
 public class Exo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner clavier = new Scanner(System.in);
+		System.out.println("Saisir une chaine");
+		String chaine = clavier.next();
+		clavier.close();
+		String inverse ="";
+		for(int i = 1; i<= chaine.length(); i++) {
+			inverse += chaine.charAt(chaine.length() - i);
+		}
+		System.out.println("l'inverse de " + chaine + " est " + inverse);
+		
 	}
-
 }
--- exoJava/src/exoJava/Cours.java
+++ exoJava/src/exoJava/Cours.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 
 public class Cours {
 	public static void main(String[] args) {
+		/*
+		 * Scanner clavier = new Scanner(System.in); int nombre = 0; do {
+		 * System.out.println("saisir un nombre entre 1 et 10"); nombre =
+		 * clavier.nextInt(); } while(nombre <1 || nombre>10); clavier.close();
+		 */
+		
 		Scanner clavier = new Scanner(System.in);
-		int nombre = 0;
-		do
-		{
-			System.out.println("saisir un nombre");
-			nombre = clavier.nextInt();
-		} while(nombre <1 || nombre>10);
+		System.out.println("saisir un nombre");
+		int n = clavier.nextInt();
 		clavier.close();
+		long fact = 1;
+		for(int i = 2; i<=n;i++)
+		{
+			fact *=1;
+		}
+		System.out.println("Factorielle de " + n + " = " +fact);
 	}
 
 }
