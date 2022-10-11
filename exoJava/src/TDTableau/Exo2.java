cb4d338a53e96c9c4be4691bf6d2dd21ba87717f
sly987 <76872198+sly987@users.noreply.github.com>
2022-10-11 16:57:13 +0200

    exo2

--- exoJava/src/TDTableau/Exo2.java
+++ exoJava/src/TDTableau/Exo2.java
@@ -10,15 +10,49 @@ public class Exo2 {
 		Scanner clavier = new Scanner(System.in);
 		Random rand = new Random();
 		int taille=0;
+		
 		do {
 			System.out.println("saisir la taille de la matrice carré entre 2 et 10");
 			taille = clavier.nextInt();
 		}while(taille<2 || taille>10);
+		
 		int [][] matrice = new int[taille][taille];
+		
 		for(int i = 0; i<taille ;  i++) {
 			for(int j = 0; j<taille; j++) {
 				matrice[i][j]= rand.nextInt(100) + 1;
+				System.out.print(matrice[i][j] + " ");
 			}
+			System.out.println("");
+		}
+		int intermédiaire = 0;
+		int middle = taille/2;
+		if(taille % 2 != 0) {
+			middle+=1;
+		}
+		for(int i = 0; i<middle ; i++) {
+			if(i==(middle-1)) {
+				for(int j = 0 ; j<middle; j++) {
+					intermédiaire = matrice[i][j];
+					matrice[i][j]= matrice[taille-1-i][taille-1-j];
+					matrice[taille-1-i][taille-1-j] =intermédiaire;
+					}
+			} else {
+				for(int j = 0 ; j<taille; j++) {
+				intermédiaire = matrice[i][j];
+				matrice[i][j]= matrice[taille-1-i][taille-1-j];
+				matrice[taille-1-i][taille-1-j] =intermédiaire;
+				}
+			}
+				
+			
+		}
+		System.out.println("");
+		for(int i = 0; i<taille ;  i++) {
+			for(int j = 0; j<taille; j++) {
+				System.out.print(matrice[i][j] + " ");
+			}
+			System.out.println("");
 		}
 	}
 
