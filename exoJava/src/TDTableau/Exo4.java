8676bab521ba296c3c2bcffa04b5c91872a714b6
sly987 <76872198+sly987@users.noreply.github.com>
2022-10-12 14:32:06 +0200

    exo 4

---
+++ exoJava/src/TDTableau/Exo4.java
@@ -0,0 +1,27 @@
+package TDTableau;
+
+public class Exo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] tab = {9,4,6,8,2,3};
+		for(int i = 0 ; i < tab.length ; i++) {
+			System.out.print(tab[i] + " ");
+		}
+		int intermédiaire;
+		for(int i = (tab.length -1) ; i >= 1 ; i-- ) {
+			for(int j = 0 ; j<= (i -1); j++) {
+				if(tab[j+1] < tab[j]) {
+					intermédiaire = tab[j];
+					tab[j] = tab[j+1];
+					tab[j+1] = intermédiaire;
+				}
+			}
+		}
+		System.out.println("");
+		for(int i = 0 ; i < tab.length ; i++) {
+			System.out.print(tab[i] + " ");
+		}
+	}
+
+}
