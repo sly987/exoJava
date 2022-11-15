a87e751c06b2bb35ec443e7c0d310206ff03bc1b
sly987 <76872198+sly987@users.noreply.github.com>
2022-11-15 11:58:58 +0100

    td classe

--- exoJava/src/Classe/Ville.java
+++ exoJava/src/Classe/Ville.java
@@ -5,24 +5,89 @@ public class Ville {
 	private String nom;
 	private String pays;
 	private int nbHabitant;
+	private char categorie;
+	private static int compteur;
 	
 	public Ville(int nbHabitant) {
+		setNbHabitant(nbHabitant);
+	}
+	
+	public Ville(String nom, String pays, int nbHabitant) {
+		this.nom = nom;
+		this.pays = pays;
+		setNbHabitant(nbHabitant);
+		Ville.compteur++;
+
+	}
+	public Ville(String nom) {
+		this(nom, "inconnu", 1000);
+	}
+	
+	public void setNbHabitant(int nbHabitant) {
 		if(nbHabitant<=10) {
 			this.nbHabitant =0;
 			System.out.println("une erreur");
 		} else {
 			this.nbHabitant = nbHabitant;
-		}	
+		}
+		setCategorie();
 	}
+	private void setCategorie() {
+		if(this.nbHabitant<1_000) {
+			categorie = 'A';
+		} else if(this.nbHabitant<10_000) {
+			categorie = 'B';
+		} else if(this.nbHabitant<100_000) {
+			categorie = 'C';
+		} else if(this.nbHabitant<500_000) {
+			categorie = 'D';
+		} else if(this.nbHabitant<1_000_000) {
+			categorie = 'E';
+		} else if(this.nbHabitant<5_000_000) {
+			categorie = 'F';
+		} else if(this.nbHabitant<10_000_000) {
+			categorie = 'G';
+		} else {
+			categorie = 'H';
+		}
+	}
+	
+	public int getNbHabitant() {
+		return this.nbHabitant;
+	}
+	
 	public void afficher() {
-		System.out.println("La ville est " + nom + " le nb d'habitant est " + nbHabitant + " le pays est " + pays);
+		System.out.println("La ville est " + nom + " le nb d'habitant est " + nbHabitant + " le pays est " + pays + " sa categorie est " + categorie);
 	}
+	
+	public void comparer(Ville v) {
+		if(this.getNbHabitant()> v.getNbHabitant()) {
+			System.out.println(this.nom + " est plus peuplé que " + v.nom);
+		} else {
+			System.out.println(v.nom + " est plus peuplé que " + this.nom);
+		}
+	}
+	public static int getCompteur() {
+		return compteur;
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Ville paris = new Ville(15_000_000);
-		paris.afficher();
-		Ville inconnu = new Ville(0);
+	
+		Ville inconnu = new Ville(1500000);
 		inconnu.afficher();
+		Ville town = new Ville("town");
+		town.afficher();
+		town = new Ville("town", "pays",999 );
+		town.afficher();
+		town = new Ville("town", "pays",9999 );
+		town.afficher();
+		town = new Ville("town", "pays",99999 );
+		town.afficher();
+		inconnu.comparer(town);
+		System.out.println(Ville.getCompteur());
 	}
+	
 
 }
